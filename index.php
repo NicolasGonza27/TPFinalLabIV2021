8847e5a5a395dfeb415bbf81a2786e08d016cde4
Nicolás Gonzalez <niclausegonzalez@hotmail.com>
2021-10-08 17:03:02 -0300

    insertando api

--- index.php
+++ index.php
@@ -15,9 +15,36 @@
 
 	session_start();
 
-	require_once(VIEWS_PATH."header.php");
+	//require_once(VIEWS_PATH."header.php");
 
-	Router::Route(new Request());
+	//Router::Route(new Request());
 
-	require_once(VIEWS_PATH."footer.php");
+	//require_once(VIEWS_PATH."footer.php");
+
+	
+
+	// $options = array(
+	// 	'http' => array(
+	// 		'method' => "GET",
+	// 		'header' => "x-api-key: 4f3bceed-50ba-4461-a910-518598664c08"
+	// 	)
+	// );
+
+	// $context = stream_context_create($options);
+	// $response = file_get_contents('https://utn-students-api.herokuapp.com/api/Student', false, $context);
+	// $arrayToDecode = json_decode($response, true);
+
+	// var_dump($arrayToDecode[0]["firstName"]);
+
+
+	use API\ApiController as ApiController;
+	use Models\Student as Student;
+
+	$ApiController = new ApiController();
+	$arrayStudents = $ApiController->GetAll();
+
+	foreach ($arrayStudents as $student) {
+		$rta = $student;
+		var_dump($rta->getFirstName());
+	}
 ?>
