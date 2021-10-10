0abd16cc3a9dc90365cb2c4e86e8a4d8b7fba7b8
Nicolás Gonzalez <niclausegonzalez@hotmail.com>
2021-10-10 00:33:37 -0300

    werwer

--- Views/company-list.php
+++ Views/company-list.php
@@ -8,7 +8,6 @@
 
             <table class="table">
                 <thead class="">
-                    
                     <tr>
                         <th class="text-center">Fantasy Name</th>
                         <th class="text-center">Country</th>
