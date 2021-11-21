75d7fe6aff8a49a74127424541bce4cd9cc4b4e3
Nicolás Gonzalez <niclausegonzalez@hotmail.com>
2021-11-21 18:09:18 -0300

    lista de postulaciones en los job offer

--- Views/jobOffer-info.php
+++ Views/jobOffer-info.php
@@ -1,15 +1,18 @@
 <?php
     $student = "";
     $admin = "";
+    $postLista = "";
     if (isset($_SESSION["student"])) {
         $student = $_SESSION["student"];
         require_once('student-nav.php');
     }
     elseif (isset($_SESSION["employer"])) {
         require_once('employer-nav.php');
+        $postLista = 1;
     }
     else {
         require_once('admin-nav.php');
+        $postLista = 1;
         $admin = 1;
     }
 ?>
@@ -75,7 +78,7 @@
 
                                         <div class="">
                                             <label for="">Insert your Curiculim here</label>
-                                            <input name="curriculum" type="file" >
+                                            <input name="curriculum" type="file" required="required">
                                         </div>
                                         <div class="">
                                             <button type="submit" class="btn btn-success d-block mr-3">Postulate</button>
@@ -103,6 +106,107 @@
                     </div>
                 </div>
             </div>
+            
+            <?php if ($postLista) { ?>
+
+            <h4 class="mt-5">Postulation List</h4>
+            <table class="table table-sm ">
+                <thead class="">
+                    <tr>
+                        <th class="text-left">Description</th>
+                        <th class="text-center">Student</th>
+                        <th class="text-center">Workload</th>
+                        <th class="text-center">Job Position</th><!-- career en detalles -->
+                        <th class="text-center">Company</th>
+                        <th class="text-center">Options</th>
+                    </tr>
+                </thead>
+                <tbody class="">
+                    <?php 
+                        if($postulationsLista) { 
+                            foreach ($postulationsLista as $postulation) {
+                                $jobOffer = $jobOfferDAO->GetOne($postulation->getJobOfferId());
+                                $jobPosition = $jobPositionDAO->GetOne($jobOffer->getJobPositionId());
+                                $company = $companyDAO->GetOne($jobOffer->getCompanyId());
+                    ?>
+                        <tr>
+                            <td class="text-left">
+                                <input class="postulationId hidden" value="<?= $postulation->getPostulationId() ?>"/>
+                                <span class="description"><?= $jobOffer->getDescription() ?></span>
+                            </td>
+                            <td class="text-left">
+                                <span class=""><?= $postulation->getStudentFullName() ?></span>
+                            </td>
+                            <td class="text-center">
+                                <span class=""><?= $jobOffer->getWorkload() ?></span>hs
+                            </td>
+                            <td class="text-center">
+                                <span class="" data-id="<?=$jobPosition->getJobPositionId()?>">
+                                    <?= $jobPosition->getDescription() ?>
+                                </span>
+                            </td>
+                            <td class="text-center">
+                                <span class="" data-id="<?=$company->getCompanyId()?>">
+                                    <?= $company->getFantasyName() ?>
+                                </span>
+                            </td>
+                            <td class="text-right">
+                                <button type="button" title="Remove" class="button-modal-delete btn btn-danger" style="margin-left: 3px;"  data-toggle="modal" data-target="#modalDeletePostulation"><i class="fas fa-trash"></i></button>
+                            </td>
+                        </tr>
+
+                    <?php   } 
+                        } 
+                        else { 
+                    ?>
+
+                        <td colspan = 7 class="text-center"><strong>THIS TABLE IS EMPTY</strong></td>              
+
+                    <?php  } ?>
+                </tbody>
+            </table>
+
+            <?php  } ?>
         </div>
     </section>
 </main>
+
+<div class="modal fade" id="modalDeletePostulation" tabindex="-1" role="dialog" aria-labelledby="editarModalLabel">
+    <div class="modal-dialog">
+        <div class="modal-content">
+
+            <form action="<?= FRONT_ROOT ?>Postulation/DeletePostulation" method="post">
+
+                <div class="modal-header">
+                    <div class="modal-title" id="exampleModalLabel">¿Are you sure to remove <strong id="postulation_name_todelete"></strong>? </div>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+
+                <input type="number" id="postulation_id_todelete" name="postulationId" value="" class="hidden">
+
+                <div class="modal-footer">
+                    <button class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                    <button type="submit" class="btn btn-danger" onclick="this.form.submit(); this.disabled=true; this.value='Sending…';">Remove</button>
+                </div>
+
+            </form>
+        </div>
+    </div>
+</div>
+
+<?php require_once(VIEWS_PATH . "footer.php"); ?>
+
+<script>
+    $(document).ready(function() {
+        $(".button-modal-delete").click(
+            function () {
+                var $row = $(this).closest("tr");
+
+                $("#postulation_name_todelete").text($row.find(".description").text());
+                $("#postulation_id_todelete").val($row.find(".postulationId").val());
+            }
+        );
+    });
+</script>
