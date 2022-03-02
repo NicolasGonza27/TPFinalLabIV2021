e5004bb981e9f9e2376bc80bff88ec30966295c0
NicolasGonza27 <66498258+NicolasGonza27@users.noreply.github.com>
2022-03-02 11:34:07 -0300

    Update mailerTemplates.php
    cambio

--- mailerTemplates.php
+++ mailerTemplates.php
@@ -59,7 +59,7 @@
                     $mail->Host       = 'smtp.gmail.com';                       // Set the SMTP server to send through
                     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
                     $mail->Username   = 'tpfinallaborato525@gmail.com';                // SMTP username
-                    $mail->Password   = 'larousse356';                    // SMTP password
+                    $mail->Password   = '';                    // SMTP password
                     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
                     $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
@@ -107,7 +107,7 @@
                 $mail->Host       = 'smtp.gmail.com';                       // Set the SMTP server to send through
                 $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
                 $mail->Username   = 'tpfinallaborato525@gmail.com';                // SMTP username
-                $mail->Password   = 'larousse356';                    // SMTP password
+                $mail->Password   = '';                    // SMTP password
                 $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
                 $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
